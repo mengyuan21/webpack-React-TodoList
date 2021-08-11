c8392d4412d11dd289e3a67cd77373b79ad5c6e8
mengyuan <15332392019@163.com>
2021-08-11 10:23:41 +0800

    [mengyuan] building

---
+++ webpack.base.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	// 入口文件
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.join(__dirname, '/dist')
+    },
+    module: {
+    	// 配置相应的规则
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }, {
+                test: /\.js[x]?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.less$/,
+                use: ['style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'less-loader'],
+            }
+        ]
+    },
+    // 配置相应的插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
+
