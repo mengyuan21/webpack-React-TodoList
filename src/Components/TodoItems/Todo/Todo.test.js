2340a497f5e977f360fc4865ef93f1bdec0feb8b
mengyuan <15332392019@163.com>
2021-08-16 21:53:27 +0800

    [mengyuan] feat: add todo test

---
+++ src/Components/TodoItems/Todo/Todo.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import {Todo,  TodoNameNormal, TodoNameComplete}from "./Todo";
+import "@testing-library/jest-dom";
+
+test("render checkbox", () => {
+    render(<Todo/>)
+    expect(screen.getByRole('checkbox')).toBeInTheDocument();
+})
+
+test("render todo name", () => {
+    const mockTodo = {
+        id:1,
+        name:'Learn Jest',
+        complete:true,
+    }
+
+    render(<Todo  todo={mockTodo}/>)
+    expect(screen.getByText('Learn Jest')).toBeInTheDocument();
+}) 
+
+// test("render delete button", () => {
+//     render(<Todo/>);
+//     expect(screen.getByRole('button')).toBeInTheDocument(); 
+// })
