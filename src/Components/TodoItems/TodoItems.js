f382dbf185c0c7f86c76e3dc458cf7cb43580fd9
mengyuan <15332392019@163.com>
2021-08-17 11:15:46 +0800

    [mengyuan] feat: test todoItems can render all todos

--- src/Components/TodoItems/TodoItems.js
+++ src/Components/TodoItems/TodoItems.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Todo from './Todo/Todo';
 
-export default function TodoItems({ displayTodos, toggleTodo, deleteTodo,editTodoItem }) {
+export default function TodoItems({ displayTodos, toggleTodo, deleteTodo }) {
     
     return (
         displayTodos.map(todo => {
             return (
-            <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo}  editTodoItem={editTodoItem} />
+            <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
             )
         })
     )
--- src/Components/TodoItems/TodoItems.test.js
+++ src/Components/TodoItems/TodoItems.test.js
@@ -2,27 +2,35 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import TodoItems from "./TodoItems"
-import Todo from './Todo/Todo';
 
-test("render todos", () => {
-    const mockTodoItems = [
-        {
-            id:1,
-            name:'learn jest',
-            complete:false,
-        },
-        {
-            id:2,
-            name:'sleep',
-            complete:false,
-        },
-        {
-            id:3,
-            name:'learn React',
-            complete:true,
-        }
-    ]
+const mockDisplayTodos = [
+    {
+        id:1,
+        name:'learn jest',
+        complete:false,
+    },
+    {
+        id:2,
+        name:'sleep',
+        complete:false,
+    },
+    {
+        id:3,
+        name:'learn React',
+        complete:true,
+    }
+]
 
-    render(<TodoItems  displayTodos={mockTodoItems}/>)
-    expect(screen.getAllByText("sleep")).toBeInTheDocument();
+
+const mocktoggleTodo = jest.fn(todo => todo.complete === !todo.complete )
+const mockDeleteTodo = jest.fn( todo => {return todo.id})
+const div = document.createElement('div');
+
+
+
+test("render all todos", () => {
+    render(<TodoItems displayTodos={mockDisplayTodos} toggleTodo={mocktoggleTodo} deleteTodo={mockDeleteTodo} />, div)
+    expect(screen.getByText('learn jest')).toBeInTheDocument();
+    expect(screen.getByText('sleep')).toBeInTheDocument();
+    expect(screen.getByText('learn React')).toBeInTheDocument();
 })
