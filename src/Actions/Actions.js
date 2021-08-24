ae1d0422e671ab6856b75a9cc70a5d0fe3a4286c
mengyuan <15332392019@163.com>
2021-08-24 18:46:24 +0800

    [mengyuan] feat: add store & Actions files

--- src/Actions/Actions.js
+++ src/Actions/Actions.js
@@ -1,8 +1,38 @@
-import * as actions from './ActionTypes'
+import * as ACTIONS from './ActionTypes'
 
-export const addTodo = (name) => ({
+export const addTodo = name => ({
     type:ACTIONS.ADD_TODOS,
     payload:{
         name
     }   
+})
+
+export const deleteTodo = id => ({
+    type: ACTIONS.DELETE_TODOS,
+    payload:{
+        id
+    }
+})
+
+export const editTodo = (id, text) => ({
+    type:ACTIONS.EDIT_TODO,
+    payload:{
+        id,
+        text
+    }
+})
+
+export const toggleTodo = (id) => ({
+    type: ACTIONS.TOGGLE_TODOS,
+    payload:{
+        id
+    }
+})
+
+export const allToComplete = () => ({
+    type: ACTIONS.ALL_TO_COMPLETE,
+})
+
+export const  clearCompleted = () => ({
+    type: ACTIONS.CLEAR_COMPLETED,
 })
